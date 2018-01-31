27e8649f9cce134aaec8bca95bb9882b7a683423
Martin Varga <martin.varga@lutraconsulting.co.uk>
2018-01-31 16:59:07 +0100

    fix sql scripts to work with postgres

--- installation/schema_pg.sql
+++ installation/schema_pg.sql
@@ -1,12 +1,11 @@
-
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
+-- This file generates tables in postgres database (public schema) required by web application
 
 --
 -- table for emails (not currently used)
 --
 
-CREATE TABLE IF NOT EXISTS attachment (
+DROP TABLE IF EXISTS attachment;
+CREATE TABLE attachment (
   id serial PRIMARY KEY,
   filename varchar(255) DEFAULT NULL,
   filelink varchar(255) DEFAULT NULL,
@@ -17,9 +16,11 @@ CREATE TABLE IF NOT EXISTS attachment (
 -- Structure for table calibration
 --
 
+DROP TYPE IF EXISTS measure;
 CREATE TYPE measure AS ENUM ('mm', 'cm', 'm');
 
-CREATE TABLE IF NOT EXISTS calibration (
+DROP TABLE IF EXISTS calibration;
+CREATE TABLE calibration (
   id serial PRIMARY KEY,
   "timestamp" timestamp with time zone DEFAULT NULL,
   height real DEFAULT NULL,
@@ -27,13 +28,13 @@ CREATE TABLE IF NOT EXISTS calibration (
   sensorid integer NOT NULL,
   yourname varchar(255) DEFAULT NULL,
   youremail varchar(255) DEFAULT NULL);
---  KEY fk_calibration_sensor1 (sensorid));
 
 --
 -- Structure for table catchment
 --
 
-CREATE TABLE IF NOT EXISTS catchments (
+DROP TABLE IF EXISTS catchment;
+CREATE TABLE catchment (
   id serial PRIMARY KEY,
   name varchar(255) NOT NULL,
   name_es varchar(255) DEFAULT NULL,
@@ -46,7 +47,8 @@ CREATE TABLE IF NOT EXISTS catchments (
 -- Structure for table email
 --
 
-CREATE TABLE IF NOT EXISTS email (
+DROP TABLE IF EXISTS email;
+CREATE TABLE email (
   id serial PRIMARY KEY,
   msgno integer DEFAULT NULL,
   sender varchar(255) DEFAULT NULL,
@@ -64,7 +66,8 @@ CREATE TABLE IF NOT EXISTS email (
 -- Table file
 --
 
-CREATE TABLE IF NOT EXISTS file (
+DROP TABLE IF EXISTS file;
+CREATE TABLE file (
   id serial PRIMARY KEY,
   filelink varchar(255) DEFAULT NULL,
   filename varchar(255) DEFAULT NULL,
@@ -78,7 +81,8 @@ CREATE TABLE IF NOT EXISTS file (
 -- Table migration
 --
 
-CREATE TABLE IF NOT EXISTS migration (
+DROP TABLE IF EXISTS migration;
+CREATE TABLE migration (
   version varchar(180) NOT NULL PRIMARY KEY,
   apply_time integer DEFAULT NULL);
 
@@ -86,10 +90,11 @@ CREATE TABLE IF NOT EXISTS migration (
 --  Table profile
 --
 
-CREATE TABLE IF NOT EXISTS profile (
+DROP TABLE IF EXISTS profile;
+CREATE TABLE profile (
   id serial PRIMARY KEY,
   user_id integer NOT NULL,
-  created_at timestamp NULL DEFAULT NULL,
+  created_at timestamp NULL DEFAULT now(),
   updated_at timestamp NULL DEFAULT NULL,
   full_name varchar(255) DEFAULT NULL);
 
@@ -97,35 +102,40 @@ CREATE TABLE IF NOT EXISTS profile (
 -- Table role
 --
 
-CREATE TABLE IF NOT EXISTS role (
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
   id serial PRIMARY KEY,
   name varchar(255) NOT NULL,
-  created_at timestamp NULL DEFAULT NULL,
+  created_at timestamp NULL DEFAULT now(),
   updated_at timestamp NULL DEFAULT NULL,
   can_admin integer NOT NULL DEFAULT '0');
 
-CREATE TABLE locations (location_id serial,
-                        location_name varchar(64),
-                        CONSTRAINT pk_locations PRIMARY KEY (location_id));
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+  location_id serial,
+  location_name varchar(64),
+  CONSTRAINT pk_locations PRIMARY KEY (location_id));
 
 --
 -- Sensors table. Note: There are two tables that need to be merged eventually; "sensors" is inherited from the IMHEA system; "sensor" is from Webvillage
 --
 
-CREATE TABLE sensors (sensor_id serial,
-                      sensor_name character varying(64),
-                      lat real,
-                      lon real,
-                      location_id integer,   
-                      catchment_id integer,       -- refers to a gauging location that closes the catchment
-                      partner varchar(64),
-		      timezone varchar(20),
-                      CONSTRAINT pk_sensors PRIMARY KEY (sensor_id),
-                      CONSTRAINT fk_catchment FOREIGN KEY (catchment_id) REFERENCES catchments,
-                      CONSTRAINT fk_location FOREIGN KEY (location_id) REFERENCES locations);
+DROP TABLE IF EXISTS sensors;
+CREATE TABLE sensors (
+  sensor_id serial,
+  sensor_name character varying(64),
+  lat real,
+  lon real,
+  location_id integer,
+  catchment_id integer,       -- refers to a gauging location that closes the catchment
+  partner varchar(64),
+  timezone varchar(20),
+  CONSTRAINT pk_sensors PRIMARY KEY (sensor_id),
+  CONSTRAINT fk_catchment FOREIGN KEY (catchment_id) REFERENCES catchment,
+  CONSTRAINT fk_location FOREIGN KEY (location_id) REFERENCES locations);
 
-
-CREATE TABLE IF NOT EXISTS sensor (
+DROP TABLE IF EXISTS sensor;
+CREATE TABLE sensor (
   id serial PRIMARY KEY,
   name varchar(255) DEFAULT NULL,
   catchmentid integer NOT NULL,
@@ -138,104 +148,110 @@ CREATE TABLE IF NOT EXISTS sensor (
   angle float DEFAULT NULL,
   property varchar(255) DEFAULT NULL,
   admin_email varchar(255) DEFAULT NULL
---  KEY fk_sensor_catchment1 (catchmentid)
 );
 
 --
 -- Table user
 --
 
-CREATE TABLE IF NOT EXISTS "user" (
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
   id serial PRIMARY KEY,
   role_id integer NOT NULL,
   status integer NOT NULL,
-  email varchar(255) DEFAULT NULL,
-  username varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL UNIQUE,
+  username varchar(255) DEFAULT NULL UNIQUE,
   password varchar(255) DEFAULT NULL,
   auth_key varchar(255) DEFAULT NULL,
   access_token varchar(255) DEFAULT NULL,
   logged_in_ip varchar(255) DEFAULT NULL,
   logged_in_at timestamp NULL DEFAULT NULL,
   created_ip varchar(255) DEFAULT NULL,
-  created_at timestamp NULL DEFAULT NULL,
+  created_at timestamp NULL DEFAULT now(),
   updated_at timestamp NULL DEFAULT NULL,
   banned_at timestamp NULL DEFAULT NULL,
   banned_reason varchar(255) DEFAULT NULL
---  UNIQUE KEY user_email (email),
---  UNIQUE KEY user_username (username),
---  KEY user_role_id (role_id)
 );
 
 --
 -- Table user_auth
 --
 
-CREATE TABLE IF NOT EXISTS user_auth (
+DROP TABLE IF EXISTS user_auth;
+CREATE TABLE user_auth (
   id serial PRIMARY KEY,
   user_id integer NOT NULL,
   provider varchar(255) NOT NULL,
   provider_id varchar(255) NOT NULL,
   provider_attributes text NOT NULL,
-  created_at timestamp NULL DEFAULT NULL,
+  created_at timestamp NULL DEFAULT now(),
   updated_at timestamp NULL DEFAULT NULL
 --  KEY user_auth_provider_id (provider_id),
 --  KEY user_auth_user_id (user_id)
 );
 
 --
--- Tabelstructuur voor tabel user_token
+-- Table user_token
 --
 
-CREATE TABLE IF NOT EXISTS user_token (
+DROP TABLE IF EXISTS user_token;
+CREATE TABLE user_token (
   id serial PRIMARY KEY,
   user_id integer DEFAULT NULL,
   type integer NOT NULL,
-  token varchar(255) NOT NULL,
+  token varchar(255) NOT NULL UNIQUE,
   data varchar(255) DEFAULT NULL,
-  created_at timestamp NULL DEFAULT NULL,
+  created_at timestamp NULL DEFAULT now(),
   expired_at timestamp NULL DEFAULT NULL
---  UNIQUE KEY user_token_token (token),
---  KEY user_token_user_id (user_id)
 );
 
 -- Other tables inherited from IMHEA
 
+DROP TABLE IF EXISTS variables;
+CREATE TABLE variables (
+  var_id serial,
+  name varchar(20),
+  description varchar(64),
+  CONSTRAINT pk_var PRIMARY KEY (var_id));
 
-CREATE TABLE variables (var_id serial,
-                        name varchar(20),
-                        description varchar(64),
-                        CONSTRAINT pk_var PRIMARY KEY (var_id));
+DROP TABLE IF EXISTS units;
+CREATE TABLE units (
+  unit_id serial,
+  name varchar(20),
+	regex varchar(20),
+  CONSTRAINT pk_units PRIMARY KEY (unit_id));
 
-CREATE TABLE units (unit_id serial,
-                    name varchar(20),
-		    regex varchar(20),
-                    CONSTRAINT pk_units PRIMARY KEY (unit_id));
+DROP TABLE IF EXISTS files;
+CREATE TABLE files (
+  file_id serial,
+  path varchar(512),
+  mtime timestamp with time zone NOT NULL,
+  uploadstatus integer,
+  CONSTRAINT pk_file PRIMARY KEY (file_id));
 
-CREATE TABLE files (file_id serial,
-                    path varchar(512),
-                    mtime timestamp with time zone NOT NULL,
-		    uploadstatus integer,
-                    CONSTRAINT pk_file PRIMARY KEY (file_id));
+DROP TABLE IF EXISTS observations;
+CREATE TABLE observations (
+  obs_id serial,
+  value numeric NOT NULL,
+  "timestamp" timestamp with time zone NOT NULL,
+  var_id integer,
+  sensor_id integer,
+	unit_id integer,
+  file_id integer,
+  CONSTRAINT pk_obs PRIMARY KEY (obs_id),
+  CONSTRAINT fk_file FOREIGN KEY (file_id) REFERENCES files,
+  CONSTRAINT fk_sensors FOREIGN KEY (sensor_id) REFERENCES sensors,
+  CONSTRAINT fk_units FOREIGN KEY (unit_id) REFERENCES units,
+  CONSTRAINT fk_var FOREIGN KEY (var_id) REFERENCES variables);
 
-CREATE TABLE observations (obs_id serial,
-                           value numeric NOT NULL,
-                           "timestamp" timestamp with time zone NOT NULL,
-                           var_id integer,  
-                           sensor_id integer,
-			   unit_id integer,
-                           file_id integer,
-                           CONSTRAINT pk_obs PRIMARY KEY (obs_id),
-                           CONSTRAINT fk_file FOREIGN KEY (file_id) REFERENCES files,
-                           CONSTRAINT fk_sensors FOREIGN KEY (sensor_id) REFERENCES sensors,
-                           CONSTRAINT fk_units FOREIGN KEY (unit_id) REFERENCES units,
-                           CONSTRAINT fk_var FOREIGN KEY (var_id) REFERENCES variables);
-
-CREATE TABLE uptime (uptime_id serial,
-                     sensor_id integer,
-                     "timestamp" timestamp with time zone NOT NULL,
-                     switch boolean,
-                     CONSTRAINT pk_uptime PRIMARY KEY (uptime_id),
-                     CONSTRAINT fk_sensors FOREIGN KEY (sensor_id) REFERENCES sensors);
+DROP TABLE IF EXISTS uptime;
+CREATE TABLE uptime (
+  uptime_id serial,
+  sensor_id integer,
+  "timestamp" timestamp with time zone NOT NULL,
+  switch boolean,
+  CONSTRAINT pk_uptime PRIMARY KEY (uptime_id),
+  CONSTRAINT fk_sensors FOREIGN KEY (sensor_id) REFERENCES sensors);
 
 
 -- Constraints
